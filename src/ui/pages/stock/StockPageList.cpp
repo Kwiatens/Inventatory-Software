2946b766edf8ab768dfa339c60c3c17b8b1cd85d
Paweł Kwiatkowski <110034652+Kwiatens@users.noreply.github.com>
2026-09-10 17:39:41 +0200

    fix(stock): remove floating category trunk

--- src/ui/pages/stock/StockPageList.cpp
+++ src/ui/pages/stock/StockPageList.cpp
@@ -65,7 +65,7 @@ ftxui::Elements App::renderStockListRows(const vector<InventorySearchMatch>& sea
                        ftxui::bgcolor(uiPanelLeftBg()));
   } else if (groupByCategory) {
     listRows.push_back(ftxui::hbox({
-                           treeCell("│   ", 4, uiDividerColor()),
+                           treeCell("", 4, uiMutedColor()),
                            treeCell("", 4, uiMutedColor()),
                            fixedCell("Part", max(1, partWidth - 8), uiMutedColor()),
                            ftxui::separator() | ftxui::color(uiDimColor()),
